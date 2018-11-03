476e5b1482addc9a524965f257c2285ecd597386
Chandru <Chandru@LAPTOP-J7JH2IG6>
2018-11-03 08:25:57 +0530

    Created maven project

---
+++ DataDrivenFramework/src/test/java/com/uttara/base/TestBase.java
@@ -0,0 +1,17 @@
+package com.uttara.base;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+
+	@BeforeSuite
+	public void setUp() {
+		
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		
+	}
+}
